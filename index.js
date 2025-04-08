1624b8d817f9745d5d5f0f0812a0f230b8063ce0
Vaibhav Bahuguna <vaibhavbahuguna423@gmail.com>
2025-04-08 20:09:58 +0530

    add files

---
+++ index.js
@@ -0,0 +1,52 @@
+import { GoogleGenAI } from "@google/genai";
+import colors from 'colors';
+import readlineSync from 'readline-sync';
+
+import { configDotenv } from "dotenv";
+
+configDotenv();
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+async function main() {
+    //Starter 
+    console.log(colors.bold.green('Welcome to the Chatbot!'))
+    console.log(colors.bold.blue('Start Chatting with the app...'))
+    console.log(colors.bold.red('Type exit to stop the chat.'))
+    
+    //Array to store chat history 
+    const chatHistory = [];
+    
+    //Continuous user chatting unit exit
+    while(true){
+        const userinput = readlineSync.question(colors.yellow('You: '));
+        try {
+            //Send stored messages to the model
+            const messages = chatHistory.map(([role, content]) => ({
+                role,
+                parts: [{ text: content }],
+            }));
+            messages.push({
+                role: "user",
+                parts: [{ text: userinput }],
+            });
+            const response = await ai.models.generateContent({
+                model: "gemini-2.0-flash",
+                contents : messages,
+            });
+            console.log(colors.green('Bot: ')+ response.text);
+
+            //type exit to kill the program
+            if(userinput.toLowerCase().includes('exit')){
+                console.log(colors.bold.blue('Feel free to ask me any questions!'))
+                break;
+            }
+            //update the chat history
+            chatHistory.push(['user', userinput]);
+            chatHistory.push(['model', response.text]);
+
+        } catch (error) {
+           console.error(colors.red(error)) 
+        }
+    }
+
+}
+main()
